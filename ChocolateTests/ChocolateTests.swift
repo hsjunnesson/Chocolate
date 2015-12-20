28406b84df9426dca825ed32a81ecf171b986ec4
Hans Sjunnesson <hans.sjunnesson@gmail.com>
2015-12-20 09:21:39 +0100

    Initial Commit

---
+++ ChocolateTests/ChocolateTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChocolateTests.swift
+//  ChocolateTests
+//
+//  Created by Hans Sjunnesson on 20/12/15.
+//  Copyright © 2015 Hans Sjunnesson. All rights reserved.
+//
+
+import XCTest
+@testable import Chocolate
+
+class ChocolateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
